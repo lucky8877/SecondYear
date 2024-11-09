a9bece45a71b57d6f24c39f95b914c52f4c69fc5
Vlaislav <itserwhite@gmail.com>
2024-11-09 14:52:26 +0300

    standart version

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,14 +1,55 @@
 template<typename T>
 class ArrayHandler {
+private:
+  T* _array;
+  size_t _len;
+  size_t _capacity;
+  T mn;
+  T mx;
+
 public:
-    void AppendElem(T elem)  {
+  ArrayHandler(size_t capacity=10) {
+    _len = 0;
+    _capacity = capacity;
+    _array = new T[_capacity];
+  }
 
+  void AppendElem(T elem) {
+    if (_len == 0) { 
+      mn = elem;
+      mx = elem;
     }
-    constexpr T GetMax()const noexcept {
-      return (sizeof(T) == 1) ? T(10): T(1000000010);      
-    }
-    constexpr T GetMin()const noexcept {
-              return (sizeof(T) == 1) ? T(1): T(0);      
 
+    if (_len == _capacity) {
+      _capacity = _capacity * 2;
+
+      T* tmp = new T[_capacity];
+      for (size_t i = 0; i < _len; i++) {
+        tmp[i] = _array[i];
+      }
+
+      delete []_array;
+      _array = tmp;
     }
+
+    if (mx < elem) {
+      mx = elem;
+    }
+    if (mn > elem) {
+      mn = elem;
+    }
+    
+    _array[_len] = elem;
+    _len++;
+  }
+
+  T GetMax() {
+    return mx;
+  }
+
+  T GetMin() {
+    return mn;
+  }
+
 };
+
